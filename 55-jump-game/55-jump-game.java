39b829d301d5b939e96e9f7dea9c3238c5aa2272
Abhishek Raj Gupta <110536209+Abhishekraj00@users.noreply.github.com>
2022-09-15 11:15:46 +0530

    Time: 13 ms (29.96%), Space: 67.9 MB (54.58%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+      
+      
+      int pos = 0 ;
+      int des = 0 ; 
+      
+      if(nums.length >= 2 && nums[0] ==0 ){
+        return false;
+      }
+      
+      if(nums.length==1){
+        return true;
+      }
+      
+      
+      for(int i=0; i<nums.length-1;i++ ){
+        
+        des = Math.max(des,nums[i]+i);
+        
+        if(pos == i){
+          pos = des;
+          
+          if(pos >= nums.length-1){
+            return true;
+          }
+        }
+        
+      }
+      
+      return false;
+        
+    }
+}
